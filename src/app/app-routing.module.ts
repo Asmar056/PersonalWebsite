d4f8043d940fc1013168a79e98b09bece29dc9fc
Asmar Ismayilova <asmar.ismayilova2@technipenergies.com>
2023-01-06 22:31:27 +0400

    home page was addes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [{
   path: '',
   component: MainComponent,
+  children: [{
+    path: '',
+    component: HomeComponent 
+  }]
 }];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './pages/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
